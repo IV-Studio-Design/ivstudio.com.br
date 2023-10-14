a88eb80e4c60d3622e9b93cdbe56d2f8c5929d91
Thales Mariano <thales.mariano125@gmail.com>
2023-10-14 00:30:58 -0300

    HomeSection created and linked

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
 	<AppHeader />
+	<HomeSection />
 </template>
 
 <script setup>
 	import AppHeader from '@/components/AppHeader.vue'
+	import HomeSection from '@/components/HomeSection.vue'
 </script>
