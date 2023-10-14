222117339385ca8d0ce81e814c55cf02e01ba38d
Thales Mariano <thales.mariano125@gmail.com>
2023-10-14 01:12:11 -0300

    AboutSection created and linked

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
 	<AppHeader />
 	<HomeSection />
+	<AboutSection />
 </template>
 
 <script setup>
 	import AppHeader from '@/components/AppHeader.vue'
 	import HomeSection from '@/components/HomeSection.vue'
+	import AboutSection from '@/components/AboutSection.vue'
 </script>
