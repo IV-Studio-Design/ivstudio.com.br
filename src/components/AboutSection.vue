e20888f95c507edb6cb5eba18aaea2881853a171
Thales Mariano <thales.mariano125@gmail.com>
2023-10-14 01:33:41 -0300

    AboutSection being added

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,3 +1,52 @@
 <template>
-	about
+	<section id="sobre" class="px-10 pb-16 bg-neutral-900">
+		<div class="w-full h-full flex gap-x-16 justify-center items-center">
+			<div class="hidden lg:block">
+				<img class="relative top-0 w-[35rem]" src="arquivos/images/cartoes-placeholder.png">
+			</div>
+			<div class="space-y-7 mt-10">
+				<div>
+					<h1 class="text-4xl/9 font-bold text-white font-montserrat whitespace-nowrap">Somos focados <br> em criar soluções <br> funcionais com <br> criatividade!</h1>
+				</div>
+				<div class="w-full max-w-[25rem] space-y-5">
+					<div class="">
+						<p class="text-white text-lg/6 font-montserrat break-words">
+							O nosso stúdio surgiu da vontade
+							e do prazer em fazer parte da história
+							de cada cliente através dos
+							nossos projetos.
+						</p>
+					</div>
+					<div class="">
+						<p class="text-white text-lg/6 font-montserrat break-words">
+							Acreditamos que o design tem o poder
+							de contar histórias, transmitir emoções 
+							e fortalecer marcas. Combinamos 
+							expertise técnica e uma 
+							arbodagem colaborativa para 
+							desenvolver projetos únicos e 
+							personalizados. 
+						</p>
+					</div>
+					<div class="">
+						<p class="text-white text-lg/6 font-montserrat break-words">
+							Desde identidades visuais até interfaces <br>
+							intuitivas e websites atraentes, estamos <br>
+							aqui para ajuda-lo a alcançar seus <br>
+							objetivos de design.
+						</p>
+					</div>
+					<div class="">
+						<p class="text-white text-lg/6 font-montserrat break-words">
+							Seja bem-vindo ao nosso stúdio de <br>
+							design, onde suas ideias ganham vida!
+						</p>
+					</div>
+				</div>
+				<div class="flex justify-center lg:justify-start items-center">
+					<a href="#" class="block font-black font-montserrat font-bold bg-white rounded-3xl px-6 py-2.5">Fale conosco!</a>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
