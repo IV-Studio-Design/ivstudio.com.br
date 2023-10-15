a3ac48d90de750e5c3c74c4061f139c1a914afc3
Thales Mariano <thales.mariano125@gmail.com>
2023-10-15 20:26:15 -0300

    Projects section created

---
+++ src/components/ProjectsSection.vue
@@ -0,0 +1,5 @@
+<template>
+	<section>
+		projects
+	</section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
 	<AppHeader />
 	<HomeSection />
 	<AboutSection />
+	<ProjectsSection />
 </template>
 
 <script setup>
 	import AppHeader from '@/components/AppHeader.vue'
 	import HomeSection from '@/components/HomeSection.vue'
 	import AboutSection from '@/components/AboutSection.vue'
+	import ProjectsSection from '@/components/ProjectsSection.vue'
 </script>
