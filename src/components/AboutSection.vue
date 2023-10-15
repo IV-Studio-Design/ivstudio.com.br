35f3c288b87d457a96ce98b693848592b8b9cd5f
Thales Mariano <thales.mariano125@gmail.com>
2023-10-15 20:23:10 -0300

    sobre styles done

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,16 +1,16 @@
 <template>
-	<section id="sobre" class="px-10 pb-16 bg-neutral-900">
+	<section id="sobre" class="px-10 bg-neutral-900 bg-iv-icon bg-cover bg-center">
 		<div class="w-full h-full flex gap-x-16 justify-center items-center">
-			<div class="hidden lg:block">
-				<img class="relative top-0 w-[35rem]" src="arquivos/images/cartoes-placeholder.png">
+			<div class="hidden md:block">
+				<img class="relative -top-[8rem] left-0 w-[30rem] lg:w-[35rem]" src="../assets/images/crachas.png">
 			</div>
 			<div class="space-y-7 mt-10">
 				<div>
-					<h1 class="text-4xl/9 font-bold text-white font-montserrat whitespace-nowrap">Somos focados <br> em criar soluções <br> funcionais com <br> criatividade!</h1>
+					<h1 class="text-center md:text-left text-3xl/9 lg:text-4xl/9 font-bold text-white font-montserrat whitespace-nowrap">Somos focados <br> em criar soluções <br> funcionais com <br> criatividade!</h1>
 				</div>
 				<div class="w-full max-w-[25rem] space-y-5">
 					<div class="">
-						<p class="text-white text-lg/6 font-montserrat break-words">
+						<p class="text-center md:text-left text-white text-base/6 lg:text-lg/6 font-montserrat break-words">
 							O nosso stúdio surgiu da vontade
 							e do prazer em fazer parte da história
 							de cada cliente através dos
@@ -18,7 +18,7 @@
 						</p>
 					</div>
 					<div class="">
-						<p class="text-white text-lg/6 font-montserrat break-words">
+						<p class="text-center md:text-left text-white text-base/6 lg:text-lg/6 font-montserrat break-words">
 							Acreditamos que o design tem o poder
 							de contar histórias, transmitir emoções 
 							e fortalecer marcas. Combinamos 
@@ -29,7 +29,7 @@
 						</p>
 					</div>
 					<div class="">
-						<p class="text-white text-lg/6 font-montserrat break-words">
+						<p class="text-center md:text-left text-white text-base/6 lg:text-lg/6 font-montserrat break-words">
 							Desde identidades visuais até interfaces <br>
 							intuitivas e websites atraentes, estamos <br>
 							aqui para ajuda-lo a alcançar seus <br>
@@ -37,13 +37,13 @@
 						</p>
 					</div>
 					<div class="">
-						<p class="text-white text-lg/6 font-montserrat break-words">
+						<p class="text-center md:text-left text-white text-base/6 lg:text-lg/6 font-montserrat break-words">
 							Seja bem-vindo ao nosso stúdio de <br>
 							design, onde suas ideias ganham vida!
 						</p>
 					</div>
 				</div>
-				<div class="flex justify-center lg:justify-start items-center">
+				<div class="flex justify-center md:justify-start items-center">
 					<a href="#" class="block font-black font-montserrat font-bold bg-white rounded-3xl px-6 py-2.5">Fale conosco!</a>
 				</div>
 			</div>
