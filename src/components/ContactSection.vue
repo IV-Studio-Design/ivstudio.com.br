b0a18fb1dfbe603e357f58d0fcb6caa98ea8fb9f
Thales Mariano <thales.mariano125@gmail.com>
2023-10-15 22:09:29 -0300

    contact section done

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,5 +1,24 @@
 <template>
-	<section>
-		Contato
+	<section id="contato" class="w-full h-full bg-black">
+		<div class="w-full h-full py-20 flex justify-center items-center px-5">
+			<div class="w-full max-w-4xl flex flex-wrap md:flex-nowrap gap-16 justify-center items-center p-10 bg-[#101010] rounded-[60px]">
+				<div class="w-64 h-80 bg-neutral-700 rounded-3xl">
+					
+				</div>
+				<div>
+					<div class="text-center md:text-left">
+						<h1 class="text-white font-montserrat font-bold text-3xl sm:text-5xl leading-8 whitespace-nowrap">Precisa de <br> um projeto?</h1>
+						<p class="text-white font-montserrat text-base sm:text-xl leading-5 mt-4 mb-5">
+							Entre em contato com a nossa equipe <br> 
+							e faça o seu orçamento! Teremos o prazer <br>
+							em participar desse projeto!
+						</p>
+						<div class="flex justify-center md:justify-start items-center">
+							<a href="#" class="block font-black text-base sm:text-lg font-montserrat font-bold bg-[#00ff7b] rounded-3xl px-8 py-2.5 whitespace-nowrap">Quero um orçamento</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
 </template>
