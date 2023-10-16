93d54464c0f4486dfe433f1edf3f18e5b10efbd9
Thales Mariano <thales.mariano125@gmail.com>
2023-10-15 21:19:46 -0300

    AboutSection padding fix

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,6 +1,6 @@
 <template>
 	<section id="sobre" class="px-10 bg-neutral-900 bg-iv-icon bg-cover bg-center">
-		<div class="w-full h-full flex gap-x-16 justify-center items-center">
+		<div class="w-full h-full flex gap-x-16 justify-center items-center py-10">
 			<div class="hidden md:block">
 				<img class="relative -top-[8rem] left-0 w-[30rem] lg:w-[35rem]" src="../assets/images/crachas.png">
 			</div>
