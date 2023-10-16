f88bc9e1fb6684a7e7471ace9a32c537b2e84589
Thales Mariano <thales.mariano125@gmail.com>
2023-10-15 21:55:14 -0300

    ContactSection component created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@
 	<AboutSection />
 	<ProjectsSection />
 
+	<!-- Partners -->
 	<div class="flex gap-x-16 gap-y-5 flex-wrap justify-center items-center bg-neutral-950 p-7">
 		<img class="w-20 sm:w-24 h-full" src="../assets/images/partners/geek_planet_logo.png" alt="Geek Planet">
 		<img class="w-44 sm:w-48 h-full" src="../assets/images/partners/thales_developer_logo.png" alt="Thales Web Developer">
@@ -12,6 +13,10 @@
 		<img class="w-44 sm:w-48 h-full" src="../assets/images/partners/jeivisselly_logo.png" alt="Jeivisselly">
 		<img class="w-16 sm:w-20 h-full" src="../assets/images/partners/damaris_toledo_logo.png" alt="Damris Toledo">
 	</div>
+	<!-- ====== -->
+
+	<ContactSection />
+
 </template>
 
 <script setup>
@@ -19,4 +24,5 @@
 	import HomeSection from '@/components/HomeSection.vue'
 	import AboutSection from '@/components/AboutSection.vue'
 	import ProjectsSection from '@/components/ProjectsSection.vue'
+	import ContactSection from '@/components/ContactSection.vue'
 </script>
