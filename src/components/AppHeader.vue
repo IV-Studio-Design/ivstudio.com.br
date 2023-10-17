c4a99b3c1b77d468a5c4d03d0a356f4e9f653462
Thales Mariano <thales.mariano125@gmail.com>
2023-10-16 22:08:31 -0300

    header styles being added

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,17 @@
 <template>
 	<header>
-		<div class="fixed top-0 left-0 z-20 w-full flex md:gap-28 lg:gap-x-36 justify-between md:justify-center items-center pt-16 px-16 transition-all">
+		<div :class="headerStyle">
 			<div>
-				<a class="hidden sm:block" href="https://ivstudio.com.br"><img id="logo" class="min-w-[13rem] w-56 lg:w-64" src="../assets/images/ivstudio-logo.png"></a>
+				<a class="hidden sm:block" href="https://ivstudio.com.br"><img id="logo" :class="logoStyle" src="../assets/images/ivstudio-logo.png"></a>
 				<a class="sm:hidden" href="https://ivstudio.com.br"><img class="w-16" src="../assets/images/iv-icon.png"></a>
 			</div>
 			<div>
 				<nav class="hidden md:block">
 					<ul class="flex space-x-7">
-						<li><a id="home-link" class="text-white relative link-selected font-montserrat font-bold text-2xl lg:text-3xl" href="#">Home</a></li>
-						<li><a id="sobre-link" class="text-white relative font-montserrat font-bold text-2xl lg:text-3xl" href="#sobre">Sobre</a></li>
-						<li><a id="projetos-link" class="text-white relative font-montserrat font-bold text-2xl lg:text-3xl" href="#projetos">Projetos</a></li>
-						<li><a id="contato-link" class="text-white relative font-montserrat font-bold text-2xl lg:text-3xl" href="#contato">Contato</a></li>
+						<li><a id="home-link" :class="navItem" href="#">Home</a></li>
+						<li><a id="sobre-link" :class="navItem" href="#sobre">Sobre</a></li>
+						<li><a id="projetos-link" :class="navItem" href="#projetos">Projetos</a></li>
+						<li><a id="contato-link" :class="navItem" href="#contato">Contato</a></li>
 					</ul>
 				</nav>
 				<div class="md:hidden">
@@ -25,3 +25,58 @@
 		</div>
 	</header>
 </template>
+
+<script setup>
+	import {ref, onMounted, onBeforeUnmount} from 'vue'
+
+	const headerStyle = ref("desactived-header")
+	const logoStyle = ref("logo-off")
+	const navItem = ref("nav-item-off")
+
+	const configHeader = event => {
+		if(!window.scrollY){
+			headerStyle.value = "desactived-header"
+			logoStyle.value = "logo-off"
+			navItem.value = "nav-item-off"
+		}else{
+			headerStyle.value = "actived-header"
+			logoStyle.value = "logo-on"
+			navItem.value = "nav-item-on"
+		}
+	}
+
+	onMounted(() => {
+		window.addEventListener('scroll', configHeader)
+	})
+
+	onBeforeUnmount(() => {
+		window.removeEventListener('scroll', configHeader)
+	})
+
+</script>
+
+<style scoped>
+	.actived-header {
+		@apply fixed top-0 left-0 z-20 w-full bg-black flex md:gap-28 lg:gap-x-36 justify-between md:justify-center items-center py-5 px-16 shadow-header transition-all
+	}
+
+	.desactived-header {
+		@apply fixed top-0 left-0 z-20 w-full flex md:gap-28 lg:gap-x-36 justify-between md:justify-center items-center pt-16 px-16 transition-all
+	}
+
+	.logo-on {
+		@apply min-w-[13rem] w-56 lg:w-56 transition-all
+	}
+
+	.logo-off {
+		@apply min-w-[13rem] w-56 lg:w-64 transition-all
+	}
+
+	.nav-item-on {
+		@apply text-white relative font-montserrat font-bold text-2xl lg:text-2xl transition-all
+	}
+
+	.nav-item-off {
+		@apply text-white relative font-montserrat font-bold text-2xl lg:text-3xl transition-all
+	}
+</style>
