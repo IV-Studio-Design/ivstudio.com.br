88373845994047fc617ca27b16fd78e9fb88fa7a
Thales Mariano <thales.mariano125@gmail.com>
2023-10-19 18:15:31 -0300

    slider done

--- src/components/PartnersSlide.vue
+++ src/components/PartnersSlide.vue
@@ -3,33 +3,43 @@
 		<div class="splide__track py-5">
 			<ul class="splide__list flex items-center">
 				<li class="splide__slide">
-					<div>
-						<img class="w-20 sm:w-24 h-full" src="../assets/images/partners/geek_planet_logo.png" alt="Geek Planet">
+					<div class="w-full flex justify-center">
+						<img class="w-20" src="../assets/images/partners/geek_planet_logo.png" alt="Geek Planet">
 					</div>
 				</li>
 				<li class="splide__slide">
-					<div>
-						<img class="w-44 sm:w-48 h-full" src="../assets/images/partners/thales_developer_logo.png" alt="Thales Web Developer">
+					<div class="w-full flex justify-center">
+						<img class="w-44" src="../assets/images/partners/thales_developer_logo.png" alt="Thales Web Developer">
 					</div>	
 				</li>
 				<li class="splide__slide">
-					<div>
-						<img class="w-28 sm:w-32 h-full" src="../assets/images/partners/vex_logo.png	" alt="Vex">
+					<div class="w-full flex justify-center">
+						<img class="w-28" src="../assets/images/partners/vex_logo.png	" alt="Vex">
 					</div>
 				</li>
 				<li class="splide__slide">
-					<div>
-						<img class="w-40 sm:w-44 h-full" src="../assets/images/partners/sidirlan_logo.png" alt="Sidirlan">
+					<div class="w-full flex justify-center">
+						<img class="w-36" src="../assets/images/partners/sidirlan_logo.png" alt="Sidirlan">
 					</div>
 				</li>
 				<li class="splide__slide">
-					<div>
-						<img class="w-44 sm:w-48 h-full" src="../assets/images/partners/jeivisselly_logo.png" alt="Jeivisselly">
+					<div class="w-full flex justify-center">
+						<img class="w-44" src="../assets/images/partners/jeivisselly_logo.png" alt="Jeivisselly">
 					</div>
 				</li>
 				<li class="splide__slide">
-					<div>
-						<img class="w-16 sm:w-20 h-full" src="../assets/images/partners/damaris_toledo_logo.png" alt="Damris Toledo">
+					<div class="w-full flex justify-center">
+						<img class="w-20" src="../assets/images/partners/damaris_toledo_logo.png" alt="Damris Toledo">
+					</div>
+				</li>
+				<li class="splide__slide">
+					<div class="w-full flex justify-center">
+						<img class="w-44" src="../assets/images/ivstudio-logo.png" alt="IV Studio">
+					</div>
+				</li>
+				<li class="splide__slide">
+					<div class="w-full flex justify-center">
+						<img class="w-20" src="../assets/images/partners/lins_fitness.png" alt="Lins Fitness">
 					</div>
 				</li>
 			</ul>
@@ -45,9 +55,11 @@
 	onMounted(() => {
 		new Splide('.splide', {
 			type: 'loop',
-			perPage: 6,
+			perPage: 9,
 			arrows: false,
 			drag: false,
+			pagination: false,
+			fixedWidth: '12rem',
 			autoScroll: {
 				speed: 3
 			}
