55e37f69a176a3af7d04621f396e14f3b95c7f71
Thales Mariano <thales.mariano125@gmail.com>
2023-10-19 18:45:04 -0300

    contact: more layout fix

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,16 +1,16 @@
 <template>
 	<section id="contato" class="w-full h-full bg-black py-5">
 		<div class="w-full h-full py-20 flex justify-center items-center px-5">
-			<div class="w-full max-w-[50rem] flex flex-wrap md:flex-nowrap gap-16 justify-center items-center p-10 bg-[#101010] rounded-[60px]">
-				<div class="w-72 h-80 rounded-3xl">
-					<img src="../assets/images/contact_banner.png">
+			<div class="w-full max-w-[55rem] py-10 flex flex-wrap md:flex-nowrap gap-x-16 gap-y-8 justify-center items-center bg-[#101010] rounded-[60px]">
+				<div class="w-64 sm:w-72 h-80 rounded-3xl">
+					<img class="w-full h-full" src="../assets/images/contact_banner.png">
 				</div>
 				<div>
-					<div class="text-center md:text-left">
+					<div class="text-center md:text-left px-8 sm:px-0">
 						<h1 class="text-white font-montserrat font-bold text-3xl sm:text-5xl leading-8 whitespace-nowrap">Precisa de <br> um projeto?</h1>
-						<p class="text-white font-montserrat text-base sm:text-xl leading-5 mt-4 mb-5">
-							Entre em contato com a nossa equipe <br> 
-							e faça o seu orçamento! Teremos o prazer <br>
+						<p class="sm:w-96 text-white font-montserrat text-base sm:text-xl leading-5 mt-4 mb-5">
+							Entre em contato com a nossa equipe
+							e faça o seu orçamento! Teremos o prazer
 							em participar desse projeto!
 						</p>
 						<div class="flex justify-center md:justify-start items-center">
