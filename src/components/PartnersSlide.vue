4f72f373a88dbbd23a103fea5d4596f218b3e5b5
Thales Mariano <thales.mariano125@gmail.com>
2023-10-19 18:35:37 -0300

    layout fixes and image removed

--- src/components/PartnersSlide.vue
+++ src/components/PartnersSlide.vue
@@ -4,7 +4,7 @@
 			<ul class="splide__list flex items-center">
 				<li class="splide__slide">
 					<div class="w-full flex justify-center">
-						<img class="w-20" src="../assets/images/partners/geek_planet_logo.png" alt="Geek Planet">
+						<img class="w-20 mr-5" src="../assets/images/partners/geek_planet_logo.png" alt="Geek Planet">
 					</div>
 				</li>
 				<li class="splide__slide">
@@ -14,12 +14,22 @@
 				</li>
 				<li class="splide__slide">
 					<div class="w-full flex justify-center">
-						<img class="w-28" src="../assets/images/partners/vex_logo.png	" alt="Vex">
+						<img class="w-20" src="../assets/images/partners/lins_fitness.png" alt="Lins Fitness">
+					</div>
+				</li>
+				<li class="splide__slide">
+					<div class="w-full flex justify-center">
+						<img class="w-28 mr-7" src="../assets/images/partners/vex_logo.png	" alt="Vex">
 					</div>
 				</li>
 				<li class="splide__slide">
 					<div class="w-full flex justify-center">
 						<img class="w-36" src="../assets/images/partners/sidirlan_logo.png" alt="Sidirlan">
+					</div>
+				</li>
+				<li class="splide__slide">
+					<div class="w-full flex justify-center">
+						<img class="w-12" src="../assets/images/partners/igordesigner.png" alt="Igor Designer">
 					</div>
 				</li>
 				<li class="splide__slide">
@@ -30,16 +40,6 @@
 				<li class="splide__slide">
 					<div class="w-full flex justify-center">
 						<img class="w-20" src="../assets/images/partners/damaris_toledo_logo.png" alt="Damris Toledo">
-					</div>
-				</li>
-				<li class="splide__slide">
-					<div class="w-full flex justify-center">
-						<img class="w-44" src="../assets/images/ivstudio-logo.png" alt="IV Studio">
-					</div>
-				</li>
-				<li class="splide__slide">
-					<div class="w-full flex justify-center">
-						<img class="w-20" src="../assets/images/partners/lins_fitness.png" alt="Lins Fitness">
 					</div>
 				</li>
 			</ul>
@@ -55,11 +55,11 @@
 	onMounted(() => {
 		new Splide('.splide', {
 			type: 'loop',
-			perPage: 9,
+			perPage: 8,
 			arrows: false,
 			drag: false,
 			pagination: false,
-			fixedWidth: '12rem',
+			fixedWidth: '10rem',
 			autoScroll: {
 				speed: 3
 			}
