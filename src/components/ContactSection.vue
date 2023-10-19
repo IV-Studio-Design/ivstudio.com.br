674b765bb96812834170c2c8912e8dcc43fa96cf
Thales Mariano <thales.mariano125@gmail.com>
2023-10-19 19:08:06 -0300

    banner fixing and link added

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -2,8 +2,8 @@
 	<section id="contato" class="w-full h-full bg-black py-5">
 		<div class="w-full h-full py-20 flex justify-center items-center px-5">
 			<div class="w-full max-w-[55rem] py-10 flex flex-wrap md:flex-nowrap gap-x-16 gap-y-8 justify-center items-center bg-[#101010] rounded-[60px]">
-				<div class="w-64 sm:w-72 h-80 rounded-3xl">
-					<img class="w-full h-full" src="../assets/images/contact_banner.png">
+				<div class="w-64 sm:w-72 h-80 bg-contact-banner bg-cover bg-center rounded-3xl">
+					<!-- image in background-image css -->
 				</div>
 				<div>
 					<div class="text-center md:text-left px-8 sm:px-0">
@@ -14,7 +14,7 @@
 							em participar desse projeto!
 						</p>
 						<div class="flex justify-center md:justify-start items-center">
-							<a href="#" class="block font-black text-base sm:text-lg font-montserrat font-bold bg-[#00ff7b] rounded-3xl px-8 py-2.5 whitespace-nowrap">Quero um orçamento</a>
+							<a href="https://wa.me/message/LKGVV4OYMYBYO1" target="_blank" class="block font-black text-base sm:text-lg font-montserrat font-bold bg-[#00ff7b] rounded-3xl px-8 py-2.5 whitespace-nowrap">Quero um orçamento</a>
 						</div>
 					</div>
 				</div>
