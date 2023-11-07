3dd3910de8970dec3f17a38d772859055fc78758
Thales Mariano <thales.mariano125@gmail.com>
2023-11-07 00:00:10 -0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import LinksView from '../views/LinksView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
