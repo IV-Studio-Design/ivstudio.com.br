550414dba262233cfd2cae13cf28cb975daf537a
Thales Mariano <thales.mariano125@gmail.com>
2023-11-06 23:31:34 -0300

    component change

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LinksView from '../views/LinksView.vue'
 
 const router = createRouter({
   mode: 'history',
@@ -13,10 +14,7 @@ const router = createRouter({
     {
       path: '/links',
       name: 'links',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/LinksView.vue')
+      component: LinksView
     }
   ]
 })
