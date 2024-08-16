330fd193295c085b1d23219dd5454fa8e6ac3d6e
Thales Mariano <thales.mariano125@gmail.com>
2024-08-15 23:35:08 -0300

    script updated && refactored

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,20 +1,20 @@
 <template>
 	<header>
-		<div :class="headerStyle">
+		<div :class="header_style">
 			<div>
-				<a class="hidden sm:block" href="https://ivstudio.com.br"><img id="logo" :class="logoStyle" src="../assets/images/ivstudio-logo.png"></a>
+				<a class="hidden sm:block" href="https://ivstudio.com.br"><img id="logo" :class="logo_style" src="../assets/images/ivstudio-logo.png"></a>
 				<a class="sm:hidden" href="https://ivstudio.com.br"><img class="w-16" src="../assets/images/iv-icon.png"></a>
 			</div>
 			<div>
 				<nav class="hidden md:block">
 					<ul class="flex space-x-7">
-						<li><a id="home-link" :class="navItem" href="#">Home</a></li>
-						<li><a id="sobre-link" :class="navItem" href="#sobre">Sobre</a></li>
-						<li><a id="projetos-link" :class="navItem" href="#projetos">Projetos</a></li>
-						<li><a id="contato-link" :class="navItem" href="#contato">Contato</a></li>
+						<li><a id="home-link" :class="nav_item" href="#">Home</a></li>
+						<li><a id="sobre-link" :class="nav_item" href="#sobre">Sobre</a></li>
+						<li><a id="projetos-link" :class="nav_item" href="#projetos">Projetos</a></li>
+						<li><a id="contato-link" :class="nav_item" href="#contato">Contato</a></li>
 					</ul>
 				</nav>
-				<div @click="openDrawer()" class="md:hidden">
+				<div @click="drawerIsOpen = true" class="md:hidden">
 					<button id="open-drawer-btn" class="space-y-1.5 mt-2">
 						<div class="w-8 h-1 bg-white rounded"></div>
 						<div class="w-8 h-1 bg-white rounded"></div>
@@ -29,7 +29,7 @@
 			>
 				<SideMenu 
 					v-show="drawerIsOpen"
-					@close="closeDrawer()"
+					@close="drawerIsOpen = false"
 				/>
 			</Transition>
 		</div>
@@ -41,24 +41,15 @@
 	import SideMenu from './SideMenu.vue'
 
 	const drawerIsOpen = ref(false)
-	const headerStyle = ref("desactived-header")
-	const logoStyle = ref("logo-off")
-	const navItem = ref("nav-item-off")
+	const header_style = ref("header desactived")
+	const logo_style = ref("logo size-off")
+	const nav_item = ref("nav-item item-off")
 
-	const configHeader = event => {
-		if(!window.scrollY){
-			headerStyle.value = "desactived-header"
-			logoStyle.value = "logo-off"
-			navItem.value = "nav-item-off"
-		}else{
-			headerStyle.value = "actived-header"
-			logoStyle.value = "logo-on"
-			navItem.value = "nav-item-on"
-		}
+	function configHeader(){
+		header_style.value = window.scrollY ? "header actived" : "header desactived"
+		logo_style.value = window.scrollY ? "logo size-on" : "logo size-off"
+		nav_item.value = window.scrollY ? "nav-item" : "nav-item item-off"
 	}
-
-	const openDrawer = () => drawerIsOpen.value = true
-	const closeDrawer = () => drawerIsOpen.value = false
 
 	onMounted(() => {
 		window.addEventListener('scroll', configHeader)
@@ -71,27 +62,35 @@
 </script>
 
 <style scoped>
-	.actived-header {
-		@apply fixed top-0 left-0 z-20 w-full bg-black flex md:gap-28 lg:gap-x-36 justify-between md:justify-center items-center py-5 px-16 shadow-header transition-all
+	.header {
+		@apply fixed top-0 left-0 z-20 w-full flex justify-between md:justify-center items-center px-16 transition-all
 	}
 
-	.desactived-header {
-		@apply fixed top-0 left-0 z-20 w-full flex md:gap-28 lg:gap-x-36 justify-between md:justify-center items-center pt-16 px-16 transition-all
+	.actived {
+		@apply bg-black md:gap-28 lg:gap-x-36 py-5 shadow-header
 	}
 
-	.logo-on {
-		@apply min-w-[13rem] w-56 lg:w-48 transition-all
+	.desactived {
+		@apply md:gap-28 lg:gap-x-36 pt-16
 	}
 
-	.logo-off {
-		@apply min-w-[13rem] w-56 lg:w-64 transition-all
+	.logo {
+		@apply min-w-[13rem] w-56 transition-all
 	}
 
-	.nav-item-on {
-		@apply text-white relative font-montserrat font-bold text-2xl lg:text-2xl transition-all
+	.size-on {
+		@apply lg:w-48
 	}
 
-	.nav-item-off {
-		@apply text-white relative font-montserrat font-bold text-2xl lg:text-3xl transition-all
+	.size-off {
+		@apply lg:w-64
+	}
+
+	.nav-item {
+		@apply text-white relative font-montserrat font-bold text-2xl transition-all
+	}
+
+	.item-off {
+		@apply lg:text-3xl
 	}
 </style>
