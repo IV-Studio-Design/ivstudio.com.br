2049f6d29bf7d19dddfe77beb75b6ffe1f4e7dd0
Thales Mariano <thales.mariano125@gmail.com>
2024-08-16 00:01:12 -0300

    New footer components added

---
+++ src/components/linksview/FooterDesktop.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full hidden sm:flex justify-between items-center px-28 py-5 sm:bg-black">
+        <div>
+			<a href="https://ivstudio.com.br"><img class="w-40" src="@/assets/images/ivstudio-logo.png"></a>
+		</div>
+		<div>
+			<p class="text-white text-center text-sm">&copy 2023 IV Studio. Todos os direitos reservados.</p>
+			<p class="text-white text-center text-sm">Desenvolvido por Thales Web Developer.</p>
+		</div>
+		<div>
+			<a href="https://thalesdeveloper.com.br" target="_blank"><img class="w-40" src="@/assets/images/thalesdeveloper-logo.png"></a>
+		</div>
+	</div>
+</template>
--- src/views/LinksView.vue
+++ src/views/LinksView.vue
@@ -66,37 +66,16 @@
 			</div>
 		</div>
 		<footer class="w-full">
-			<div class="w-full flex flex-col gap-y-5 sm:hidden">
-				<div class="w-full">
-					<p class="text-white text-center text-xs">&copy 2023 IV Studio. Todos os direitos reservados.</p>
-					<p class="text-white text-center text-xs">Desenvolvido por Thales Web Developer.</p>
-				</div>
-				<div class="w-full flex justify-center items-center gap-x-4">
-					<div>
-						<a href="https://ivstudio.com.br"><img class="w-28" src="@/assets/images/ivstudio-logo.png"></a>
-					</div>
-					<div class="w-px h-8 bg-white"></div>
-					<div>
-						<a href="https://thalesdeveloper.com.br" target="_blank"><img class="w-28" src="@/assets/images/thalesdeveloper-logo.png"></a>
-					</div>
-				</div>
-			</div>
-
-			<div class="w-full hidden sm:flex justify-between items-center px-28 py-5 sm:bg-black">
-				<div>
-					<a href="https://ivstudio.com.br"><img class="w-40" src="@/assets/images/ivstudio-logo.png"></a>
-				</div>
-				<div>
-					<p class="text-white text-center text-sm">&copy 2023 IV Studio. Todos os direitos reservados.</p>
-					<p class="text-white text-center text-sm">Desenvolvido por Thales Web Developer.</p>
-				</div>
-				<div>
-					<a href="https://thalesdeveloper.com.br" target="_blank"><img class="w-40" src="@/assets/images/thalesdeveloper-logo.png"></a>
-				</div>
-			</div>
+			<FooterDesktop/>
+			<FooterMobile/>
 		</footer>
 	</div>
 </template>
+
+<script setup>
+	import FooterDesktop from '@/components/linksview/FooterDesktop.vue'
+	import FooterMobile from '@/components/linksview/FooterMobile.vue'
+</script>
 
 <style scoped>
 	.blurry-eye-iv-image {
