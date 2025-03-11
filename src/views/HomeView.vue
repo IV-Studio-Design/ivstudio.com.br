b0ed53e1e4c06c961b8e03277c3ec8cdbf10e121
= <thales.mariano125@gmail.com>
2025-03-11 11:50:35 -0300

    feat: restart project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,9 @@
-<template>
-	<AppHeader />
-	<HomeSection />
-	<AboutSection />
-	<ProjectsSection />
-	<PartnersSlide />
-	<ContactSection />
-	<AppFooter />
-
-</template>
-
-<script setup>
-	import AppHeader from '@/components/AppHeader.vue'
-	import HomeSection from '@/components/HomeSection.vue'
-	import AboutSection from '@/components/AboutSection.vue'
-	import ProjectsSection from '@/components/ProjectsSection.vue'
-	import PartnersSlide from '@/components/PartnersSlide.vue'
-	import ContactSection from '@/components/ContactSection.vue'
-	import AppFooter from '@/components/AppFooter.vue'
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
